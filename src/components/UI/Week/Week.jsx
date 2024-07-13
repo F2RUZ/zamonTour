a7f8f6deb6b0cf6fd4ce419c9986a1bf7b7bde05
F2RUZ <gaffarovferuz4@gmail.com>
2024-07-14 02:53:14 +0500

    Week

--- src/components/UI/Week/Week.jsx
+++ src/components/UI/Week/Week.jsx
@@ -11,12 +11,40 @@ function Week() {
   var settings = {
     dots: true,
     infinite: true,
+    speed: 500,
     slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    pauseOnHover: true,
-  };
+    slidesToScroll: 4,
+    initialSlide: 0,
+    
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+            }
+        },
+
+   
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                initialSlide: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+};
   return (
     <div className="week">
       <div className="week__wrapper">
