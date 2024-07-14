076c0496a284246f2118be72ff0b12fea147673c
F2RUZ <gaffarovferuz4@gmail.com>
2024-07-14 05:57:10 +0500

    location

--- src/components/UI/Locations/Location.jsx
+++ src/components/UI/Locations/Location.jsx
@@ -1,11 +1,73 @@
-import React from 'react'
-import './location.scss'
+import React from "react";
+import "./location.scss";
 const Location = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className="location">
+      <div className="location__wrapper">
+        <input type="radio" name="slide" id="c1" checked />
+        <label htmlFor="c1" className="card">
+          <div className="row">
+            <div className="icon">
+              <i className="fa-regular fa-square-plus"></i>
+            </div>
+            <div className="description">
+              <h4>Nyu York</h4>
+              <p>Population: 2M</p>
+            </div>
+          </div>
+        </label>
+        <input type="radio" name="slide" id="c2" />
+        <label htmlFor="c2" className="card">
+          <div className="row">
+            <div className="icon">
+              <i className="fa-regular fa-square-plus"></i>
+            </div>
+            <div className="description">
+              <h4>Antalya</h4>
+              <p>Population: 3.5M</p>
+            </div>
+          </div>
+        </label>
+        <input type="radio" name="slide" id="c3" />
+        <label htmlFor="c3" className="card">
+          <div className="row">
+            <div className="icon">
+              <i className="fa-regular fa-square-plus"></i>
+            </div>
+            <div className="description">
+              <h4>Istanbul</h4>
+              <p>Population: 4.1M</p>
+            </div>
+          </div>
+        </label>
+        <input type="radio" name="slide" id="c4" />
+        <label htmlFor="c4" className="card">
+          <div className="row">
+            <div className="icon">
+              <i className="fa-regular fa-square-plus"></i>
+            </div>
+            <div className="description">
+              <h4>Sharm El-Sheikh</h4>
+              <p>Population: 3M</p>
+            </div>
+          </div>
+        </label>
 
-export default Location
+        <input type="radio" name="slide" id="c5" />
+        <label htmlFor="c5" className="card">
+          <div className="row">
+            <div className="icon">
+              <i className="fa-regular fa-square-plus"></i>
+            </div>
+            <div className="description">
+              <h4>Sharm El-Sheikh</h4>
+              <p>Population: 3M</p>
+            </div>
+          </div>
+        </label>
+      </div>
+    </div>
+  );
+};
+
+export default Location;
